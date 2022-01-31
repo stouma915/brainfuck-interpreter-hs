0bcaa29985818f7cedeb30dd7e59cce3fe8d45d7
stouma915 <sasakitouma@yahoo.ne.jp>
2022-01-31 17:09:39 +0900

    指定されたファイルの内容を読み込めるように

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import System.Environment (getArgs, getProgName)
+import System.Exit (ExitCode(ExitFailure), exitWith, exitSuccess)
+import System.IO ()
+
+import Control.Exception (try)
+import GHC.IO.Exception (ioe_description)
+
 main :: IO ()
-main = putStrLn "33-4"
+main = do
+  args <- getArgs
+  progName <- getProgName
+
+  if null args then do
+    putStrLn "Brainf**k Interpreter 1.0.0"
+    putStrLn ""
+    putStrLn "Usage:"
+    putStrLn $ "    " ++ progName ++  " [SOURCE]"
+    putStrLn ""
+    putStrLn "Args:"
+    putStrLn "<SOURCE> Brainfuck source file."
+  else do
+    let sourceFileName = head args
+
+    contentOrErr <- try (readFile sourceFileName) :: IO (Either IOError String)
+    case contentOrErr of
+      Left err -> do
+        putStrLn $ "Unable to read source file: " ++ ioe_description err
+
+        exitWith (ExitFailure 2)
+      Right content -> do
+        putStrLn content
+
+        exitSuccess
