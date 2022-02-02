508b317e9f4a8c78a44cf161e850b3fa5e9acfdb
stouma915 <sasakitouma@yahoo.ne.jp>
2022-02-02 15:47:58 +0900

    インクリメント、デクリメントを実装

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,14 +1,44 @@
 module Interpreter(eval) where
 
+import qualified Data.Map.Strict as M
+
 import Control.Monad
 import Data.IORef
+import Data.Maybe
 
 eval :: String -> Int -> [(Int, Int)] -> IO String
 eval sourceCode memoryPointer memoryEntries = do
+  memoryRef <- newIORef $ M.fromList memoryEntries
   pointerRef <- newIORef memoryPointer
 
   forM_ sourceCode $ \char -> do
     case char of
+      '+' -> do
+        currentMemory <- readIORef memoryRef
+        currentPointer <- readIORef pointerRef
+
+        if isNothing (currentMemory M.!? currentPointer) then
+          modifyIORef memoryRef (M.insert currentPointer 1)
+        else do
+          let currentValue = currentMemory M.! currentPointer
+
+          if currentValue >= 255 then
+            modifyIORef memoryRef (M.update (\_ -> Just 0) currentPointer)
+          else
+            modifyIORef memoryRef (M.update (\x -> Just(x + 1)) currentPointer)
+      '-' -> do
+        currentMemory <- readIORef memoryRef
+        currentPointer <- readIORef pointerRef
+
+        if isNothing (currentMemory M.!? currentPointer) then
+          modifyIORef memoryRef (M.insert currentPointer 255)
+        else do
+          let currentValue = currentMemory M.! currentPointer
+
+          if currentValue <= 0 then
+            modifyIORef memoryRef (M.update (\_ -> Just 255) currentPointer)
+          else
+            modifyIORef memoryRef (M.update (\x -> Just(x - 1)) currentPointer)
       '>' ->
         modifyIORef pointerRef (+ 1)
       '<' -> do
